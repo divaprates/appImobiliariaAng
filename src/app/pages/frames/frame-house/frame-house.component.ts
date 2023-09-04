14b5000d6c6808e5a662a28358c3e34ca8ae33b7
divaprates <divasilvaprates@gmail.com>
2023-09-04 20:04:39 -0300

    style: organizando gitignore

--- src/app/pages/frames/frame-house/frame-house.component.ts
+++ src/app/pages/frames/frame-house/frame-house.component.ts
@@ -11,10 +11,31 @@ export class FrameHouseComponent implements OnInit{
   constructor(){}
 
   ngOnInit(): void {
-    this.linksList = ['https://www.oliveiraimoveis.net/aluguel/casa/',
-      'https://www.cirsa.com.br/aluguel/casa/todas-as-cidades/todos-os-bairros/0-quartos/0-suite-ou-mais/0-vaga/0-banheiro-ou-mais/todos-os-condominios?valorminimo=0&valormaximo=0&pagina=1',
-      'https://www.oliveiraimoveis.net/aluguel/casa/',
-      'https://www.oliveiraimoveis.net/aluguel/casa/'];
+    this.linksList = [
+      // 'https://www.visaoimoveis.net/aluguel/casa/todas-as-cidades/todos-os-bairros/0-quartos/0-suite-ou-mais/0-vaga/0-banheiro-ou-mais/todos-os-condominios?valorminimo=0&valormaximo=0&pagina=1'
+      
+      'https://www.dommusimoveis.com.br/imoveis/para-alugar/casa'
+      
+      ,'https://www.cirsa.com.br/aluguel/casa--cobertura/todas-as-cidades/todos-os-bairros/0-quartos/0-suite-ou-mais/0-vaga/0-banheiro-ou-mais/todos-os-condominios?valorminimo=0&valormaximo=0&pagina=1'
+      
+      ,'https://www.oliveiraimoveis.net/aluguel/casa+apartamento-duplex+cobertura/'
+      
+      ,'http://www.realizeipatinga.com.br/pesquisar-imovel/?status=aluguel&type=casa'
+      
+      ,'https://www.pontualimobiliaria.com.br/buscar?order=neighborhood&availability=rent&city=Ipatinga&city=Coronel%20Fabriciano&property_type=Casa'
+      
+      ,'https://www.carlaoimoveismg.com.br/imovel#imoveisbusca'
+      
+      ,'https://www.gustavodruwe.com.br/property-search/?status=aluguel&type=casa'
+      
+      ,'https://locasaimoveismg.com.br/pesquisar-imoveis/'
+      
+      ,'https://lageimoveis.com.br/#/properties'
+      
+      ,'https://lageimoveis.com.br/#/properties'
+      
+      ,'https://mercantilimoveis.com.br/pesquisa-imoveis/?status%5B%5D=aluguel&type%5B%5D=casa]'
+    ]
   }
 
 }
